afa420cbc9a83f30a5d893c99d24e973a329c348
Lukáš Lalinský <lalinsky@gmail.com>
2021-12-30 07:09:33 +0100

    Add missing files

---
+++ sql/acoustid_fingerprint_encode.sql
@@ -0,0 +1,2 @@
+SELECT acoustid_fingerprint_encode('{}v1'::acoustid_fingerprint);
+SELECT acoustid_fingerprint_encode('{198178303,198047167,164426175}v1'::acoustid_fingerprint);
---
+++ sql/acoustid_fingerprint_encode_binary.sql
@@ -0,0 +1,2 @@
+SELECT acoustid_fingerprint_encode_binary('{}v1'::acoustid_fingerprint);
+SELECT acoustid_fingerprint_encode_binary('{198178303,198047167,164426175}v1'::acoustid_fingerprint);
